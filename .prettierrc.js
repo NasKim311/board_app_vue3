9f4f7bd053b4a31d2db906c23e0ac2271d5d9bc6
김남수 <skatn0311@gmail.com>
2025-03-19 15:52:22 +0900

    init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    singleQuote: true, // 문자열은 따옴표로 formatting
+    semi: true, //코드 마지막에 세미콜른이 없게 formatting
+    useTabs: false, //탭의 사용을 금하고 스페이스바 사용으로 대체하게 formatting
+    tabWidth: 4, // 들여쓰기 너비는 4칸
+    trailingComma: 'all', // 자세한 설명은 구글링이 짱이긴하나 객체나 배열 키:값 뒤에 항상 콤마를 붙히도록 formatting
+    printWidth: 250, // 코드 한줄 maximum칸
+};
