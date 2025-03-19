8e915b6528f6f8491d29035d64ad3b571bdea73e
김남수 <skatn0311@gmail.com>
2025-03-19 16:51:52 +0900

    필요없는 파일 삭제

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
