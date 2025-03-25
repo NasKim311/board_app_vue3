ebcc40c2e9b763a9d134b183c2516479e7d44b81
김남수 <skatn0311@gmail.com>
2025-03-25 16:51:36 +0900

    element-plus 라이브러리 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@/assets/css/style.css';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -11,5 +13,6 @@ import router from '@/router';
 const app = createApp(App);
 
 app.use(router);
+app.use(ElementPlus);
 
 app.mount('#app');
