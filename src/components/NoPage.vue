9e6c228445415635c59f7b00882fb07a2332356f
김남수 <skatn0311@gmail.com>
2025-03-25 11:22:30 +0900

    NoPage.vue 생성

--- src/components/NoPage.vue
+++ src/components/NoPage.vue
@@ -1,7 +1,17 @@
 <template>
-    <div></div>
+    <div class="text-center py-5">
+        <h1>404 Not Found</h1>
+        <div class="text-muted">요청한 페이지를 찾을 수 없습니다.</div>
+        <div class="mt-4">
+            <RouterLink to="/">
+                <button class="btn btn-primary">Home</button>
+            </RouterLink>
+        </div>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
 
 <style scoped></style>
