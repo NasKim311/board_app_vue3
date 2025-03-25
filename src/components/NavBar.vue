7a07ed6a71ccf0b894e4dc0c5168ef4713e8c0e1
김남수 <skatn0311@gmail.com>
2025-03-25 12:00:54 +0900

    NavBar 작업

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,19 @@
 <template>
-    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-        <ul className="navbar-nav">
-            <li className="nav-item relative">
-                <a className="nav-link ic_expend" data-widget="pushmenu" href="#" role="button">
-                    <i className="fas fa-bars"></i>
-                </a>
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light p-2">
+        <ul class="navbar-nav">
+            <li class="nav-item relative">
+                <button class="nav-link ic_expend" @click="onToggle" role="button">
+                    <i class="fas fa-bars"></i>
+                </button>
             </li>
         </ul>
     </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function onToggle() {
+    document.body.classList.toggle('sidebar-collapse');
+}
+</script>
 
 <style scoped></style>
